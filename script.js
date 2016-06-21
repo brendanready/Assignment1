b729d941f10ea6c30eada1a4052ffb20f2196d1f
brendanready <brendanbready@gmail.com>
2016-06-20 18:41:09 -0700

    New files added to Assignment 1 master branch

---
+++ script.js
@@ -0,0 +1,193 @@
+/*
+        Brendan Ready
+        Assignment 1
+        FEWD 2
+*/
+
+// STEP 1
+/*
+var someMonth;
+function theMonth           // function to return current month
+currentMonth                // a constant
+var summerMonth;            // an array of summer months
+myLibraryAFunction          // a function
+*/
+
+//STEP 2
+/*
+21.9                        // numeric literal expression
+"Hello Zak"                 // string literal expression
+true                        // Boolean literal expression
+null                        // null literal expression
+*/
+
+//STEP 3
+/*
+var totalProducts = 3 * 6;
+var welcomeMsg = "Hello" + firstName;
+*/
+
+//STEP 4
+/*
+var sFirstName, sLastName, sAddress, sCity, sState, nZipCode, nYourAge, sReferralSource, bMayWeContactYou;
+*/
+
+//STEP 5
+/*
+var sFirstName;
+var sLastName;
+sFirstName = "Brendan";
+sLastName = "Ready";
+
+var sAddress = "123 Park Ave.";
+var sCity = "San Diego";
+var sState = "CA";
+var sZipCode = "92101";
+
+var nYourAge = 6, sReferralSource = "Google", bMayWeContactYou = true;
+*/
+
+//STEP 6
+/*
+var numString = 123 + " Park Ave.";
+console.log(numString);
+
+var booString = true + " May We Contact You?";
+console.log(booString);
+
+var NumbBoo = true + 6;
+console.log(NumbBoo);
+*/
+
+//STEP 7
+/*
+var firstName = "Brendan";
+console.log("My name is " + firstName + " " + lastName + ".");
+var lastName = "Ready";
+*/
+
+//STEP 8
+/*
+// No, it's not valid.  You must add a backslash to "I'm" to make it "I\'m".
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+//STEP 9
+/*
+var time = null;
+console.log(time);                                  //null value returned
+
+var speed;
+console.log(speed);                                 //undefined value returned
+*/
+
+//STEP 10
+/*
+console.log(typeof "Brendan Ready");                //returns string
+console.log(typeof 6);                              //returns number
+console.log(typeof true);                           //returns boolean
+console.log(typeof {city:'San Diego', zip:92027});  //returns object
+console.log(typeof myName);                         //returns undefined
+*/
+
+//STEP 11
+/*
+alert("Hello " + "Brendan Ready" + ", welcome to the JavaScript class!");
+*/
+
+//STEP 12
+/*
+var name = "Brendan Ready";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+var name = "Brendan Ready";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+//STEP 14
+/*
+var name = "Brendan Ready";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 15
+/*
+var name = prompt('What is your name?');
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 16
+/*
+var name = prompt('What is your name?');
+var course = prompt('What class are you taking?');
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 17
+/*
+var x = 10;
+var y = 20;
+console.log(x + y);
+*/
+
+//STEP 18
+/*
+var x = 20;
+x += 20;
+console.log(x);
+*/
+
+//STEP 19
+/*
+var x = 20;
+x *= 5;
+console.log(x);
+*/
+
+//STEP 20
+/*
+var x = 20 % 3;
+x /= 1;
+console.log(x);
+*/
+
+//STEP 21
+/*
+var x = 5;
+var y = 1;
+console.log(x != y || x < y);       //evaluates to true
+*/
+
+//STEP 22
+/*
+var x = 1;
+var y = 5;
+var z = "1";
+console.log(x === z && y == 5);     //evaluates to false
+*/
+
+//STEP 23
+/*
+var widget = new Object();
+console.log(typeof widget);
+*/
+
+//STEP 24
+/*
+var widget = new Object();
+console.log(widget instanceof Object);  //returns true
+*/
+
+//STEP 25
+/*
+var widget = new Object();
+console.log(widget instanceof Number);  //returns false
+*/
